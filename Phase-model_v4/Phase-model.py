54fc92c62b864c17e3c86d2680fb8369197d4445
junlou <2190921493@qq.com>
2024-03-23 18:33:28 +0200

    update parameter

--- Phase-model_v4/Phase-model.py
+++ Phase-model_v4/Phase-model.py
@@ -7,16 +7,94 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 import logging
+import seaborn as sns
 ##from tqdm import tqdm
 
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+#os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 # config log
-logging.basicConfig(filename='log/V4_training_bs64_epo10_02.log', level=logging.INFO,
+logging.basicConfig(filename='log/V4_training_bs64_layer2_03.log', level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(message)s')
 # put device on GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Training on {device}")
+
+skip_training = False
+
+
+def matrix():
+    model.eval()
+    labels = []
+    preds = []
+    with torch.no_grad():
+        for phase, label in test_loader:
+            phase, label = phase.to(device), label.to(device)
+            outputs = model(phase)
+            _, predicted = torch.max(outputs.data, 1)
+
+            # Collect the true and predicted labels
+            labels.extend(label.cpu().numpy())
+            preds.extend(predicted.cpu().numpy())
+
+        # Compute the confusion matrix
+        cm = confusion_matrix(labels, preds)
+        print(cm)
+
+        cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print(cm_normalized)
+
+        # Plot the confusion matrix
+        fig, ax = plt.subplots(figsize=(10, 10))
+        sns.heatmap(cm_normalized, annot=False, fmt='.4f', ax=ax, cmap='Blues',  # Turn off automatic annotation
+                    xticklabels=classes, yticklabels=classes)
+
+        # Manually add annotations
+        for i in range(len(classes)):
+            for j in range(len(classes)):
+                ax.text(j + 0.5, i + 0.5, '{:0.4f}'.format(cm_normalized[i, j]),
+                        horizontalalignment='center', verticalalignment='center',
+                        color="black" if cm_normalized[i, j] < 0.5 else "white")
+
+        plt.ylabel('Actual')
+        plt.xlabel('Predicted')
+        plt.title('Confusion Matrix')
+        plt.savefig(f"./plot/Phasemodel_v4_matrix_layer2.png")
+
+    print('Finished drawing')
+def matrix_old():
+    model.eval()
+    labels = []
+    preds = []
+    with torch.no_grad():
+        for phase, label in test_loader:
+            phase, label = phase.to(device), label.to(device)
+            outputs = model(phase)
+            _, predicted = torch.max(outputs.data, 1)
+
+            # 收集预测和真实标签
+            labels.extend(label.cpu().numpy())
+            preds.extend(predicted.cpu().numpy())
+
+        # 计算混淆矩阵
+        cm = confusion_matrix(labels, preds)
+        print(cm)
+
+        cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print(cm_normalized)
+        # 绘制混淆矩阵
+        fig, ax = plt.subplots(figsize=(10, 10))
+        sns.heatmap(cm_normalized, annot=True, fmt='.4f', ax=ax, cmap='Blues',
+                    xticklabels=classes, yticklabels=classes)
+
+        plt.ylabel('Actual')
+        plt.xlabel('Predicted')
+        plt.title('Confusion Matrix - Phase only')
+        plt.savefig(f"./plot/Phasemodel_v4_matrix_e.png")
+
+    print('Finished drawing')
 
 # Load data
 # data_array = np.load('time_series_data.npy')
@@ -42,7 +120,6 @@ data_scaled = scaler.fit_transform(data_array)
 # mean = data_array.mean()
 # std = data_array.std()
 # data_normalized = (data_array - mean) / std
-
 
 # Preprocess labels
 label_encoder = LabelEncoder()
@@ -77,54 +154,62 @@ class LSTMModel(nn.Module):
         predictions = self.linear(lstm_out[:, -1])
         return predictions
 
+if __name__ == "__main__":
 
+    # Training setup
+    model = LSTMModel().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.0005)  # Adjust learning rate if necessary
 
-# Training setup
-model = LSTMModel().to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)  # Adjust learning rate if necessary
+    classes = ["LIFT", "LR", "PUSH", "PULL"]
 
-# Training loop
-num_epochs = 100  # Adjust number of epochs if necessary
-for epoch in range(num_epochs):
-    # train_loop = tqdm(train_loader, leave=True, miniters=1)
-    for inputs, labels in train_loader:
-        inputs = inputs.to(device)
-        labels = labels.to(device)
+    # Training loop
+    num_epochs = 300  # Adjust number of epochs if necessary
+    train_acc_array = []
+    test_acc_array = []
 
-        optimizer.zero_grad()
-        output = model(inputs)
-        loss = criterion(output, labels)
-        loss.backward()
-        optimizer.step()
-    print(f'Epoch {epoch+1}, Loss: {loss.item()}')
-    logging.info(f'Epoch {epoch+1}, Loss: {loss.item()}')
+    if not skip_training:
+        for epoch in range(num_epochs):
+            # train_loop = tqdm(train_loader, leave=True, miniters=1)
+            model.train()
+            for inputs, labels in train_loader:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
-# Testing
-correct = 0
-total = 0
-with torch.no_grad():
-    for inputs, labels in test_loader:
-        inputs = inputs.to(device)
-        labels = labels.to(device)
+                optimizer.zero_grad()
+                output = model(inputs)
+                loss = criterion(output, labels)
+                loss.backward()
+                optimizer.step()
+            print(f'Epoch {epoch+1}, Loss: {loss.item()}')
+            logging.info(f'Epoch {epoch+1}, Loss: {loss.item()}')
 
-        outputs = model(inputs)
-        _, predicted = torch.max(outputs, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+    # Testing
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
 
-print(f'Accuracy: {100 * correct / total}%')
-logging.info(f'Accuracy: {100 * correct / total}%')
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-torch.save(model.state_dict(), "parameter/Phase_LSTM2_parameter_02.pt")
-# print("Model's state_dict:")
-# model.eval()
-# for param_tensor in model.state_dict():
-#     print(param_tensor, "\t", model.state_dict()[param_tensor].size())
-#
-# model_clone = LSTMModel()
-# model_clone.load_state_dict(torch.load("./Phase_LSTM2_parameter.pt"))
-# print("Model_clone's state_dict:")
-# model_clone.eval()
-# for param_tensor in model_clone.state_dict():
-#     print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+    print(f'Accuracy: {100 * correct / total}%')
+    logging.info(f'Accuracy: {100 * correct / total}%')
+
+    torch.save(model.state_dict(), "parameter/Phase_LSTM2_parameter_03.pt")
+    matrix()
+    # print("Model's state_dict:")
+    # model.eval()
+    # for param_tensor in model.state_dict():
+    #     print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+    #
+    # model_clone = LSTMModel()
+    # model_clone.load_state_dict(torch.load("./Phase_LSTM2_parameter.pt"))
+    # print("Model_clone's state_dict:")
+    # model_clone.eval()
+    # for param_tensor in model_clone.state_dict():
+    #     print(param_tensor, "\t", model.state_dict()[param_tensor].size())
--- RSS-model_v4/RSS_model.py
+++ RSS-model_v4/RSS_model.py
@@ -4,18 +4,61 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset, random_split
 import numpy as np
-import logging
 from sklearn.preprocessing import LabelEncoder
-
-os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import logging
+import seaborn as sns
+#os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 # config log
-logging.basicConfig(filename='log/V4_training_bs64_epo100_layer2_01.log', level=logging.INFO,
+logging.basicConfig(filename='log/V4_training_bs64_epo100_layer2_02.log', level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(message)s')
 
 # put device on GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Training on {device}")
+def matrix():
+    model.eval()
+    labels = []
+    preds = []
+    with torch.no_grad():
+        for RSS, label in test_loader:
+            RSS, label = RSS.to(device), label.to(device)
+            outputs = model(RSS)
+            _, predicted = torch.max(outputs.data, 1)
 
+            # Collect the true and predicted labels
+            labels.extend(label.cpu().numpy())
+            preds.extend(predicted.cpu().numpy())
+
+        # Compute the confusion matrix
+        cm = confusion_matrix(labels, preds)
+        print(cm)
+
+        cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print(cm_normalized)
+
+        # Plot the confusion matrix
+        fig, ax = plt.subplots(figsize=(10, 10))
+        sns.heatmap(cm_normalized, annot=False, fmt='.4f', ax=ax, cmap='Blues',  # Turn off automatic annotation
+                    xticklabels=classes, yticklabels=classes)
+
+        # Manually add annotations
+        for i in range(len(classes)):
+            for j in range(len(classes)):
+                ax.text(j + 0.5, i + 0.5, '{:0.4f}'.format(cm_normalized[i, j]),
+                        horizontalalignment='center', verticalalignment='center',
+                        color="black" if cm_normalized[i, j] < 0.5 else "white")
+
+        plt.ylabel('Actual')
+        plt.xlabel('Predicted')
+        plt.title('Confusion Matrix -RSS only')
+        plt.savefig(f"./plot/RSS_v4_matrix_e.png")
+
+    print('Finished drawing')
 # Load data
 # data_array = np.load('time_series_data.npy')
 # label_array = np.load('labels.npy')
@@ -65,8 +108,12 @@ model = LSTMModel().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)  # Adjust learning rate if necessary
 
+classes = ["LIFT", "LR", "PUSH", "PULL"]
+
 # Training loop
-num_epochs = 100  # Adjust number of epochs if necessary
+num_epochs = 300  # Adjust number of epochs if necessary
+train_acc_array = []
+test_acc_array = []
 for epoch in range(num_epochs):
     for inputs, labels in train_loader:
         inputs = inputs.to(device)
@@ -96,8 +143,8 @@ with torch.no_grad():
 print(f'Accuracy: {100 * correct / total}%')
 logging.info(f'Accuracy: {100 * correct / total}%')
 torch.save(model.state_dict(), "parameter/RSS_LSTM2_parameter.pt")
-
-net_clone = LSTMModel()
-net_clone.load_state_dict(torch.load("parameter/RSS_LSTM2_parameter.pt"))
+matrix()
+#net_clone = LSTMModel()
+#net_clone.load_state_dict(torch.load("parameter/RSS_LSTM2_parameter.pt"))
 
 #torch.sigmoid(net_clone.forward(torch.tensor(test_dataset[0:10]).float())).data
